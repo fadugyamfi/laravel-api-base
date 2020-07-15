ea71fcd89af564c93eae02a6307c0c465307aa9a
Francis Adu-Gyamfi <francis.adu-gyamfi@topgolf.com>
2020-07-15 02:07:59 +0000

    Updated function documentation

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -30,7 +30,7 @@ abstract class ApiController extends BaseController
 
     public function __construct(CommonModel $model, String $resource = null) {
         $this->Model = $model;
-        
+
         $this->setResource($resource);
     }
 
@@ -59,7 +59,7 @@ abstract class ApiController extends BaseController
         } catch(\Error | \Exception $e) {
             $this->Resource = $packageResNS . "\\ApiResource";
         }
-        
+
         $this->Request = $requestNS . "\\" . $modelClassName . 'Request';
 
         try {
@@ -74,13 +74,13 @@ abstract class ApiController extends BaseController
     /**
      * Get All
      *
-     * @queryParam limit Total items to return e.g. `?limit=15`
-     * @queryParam page Page of items to return e.g. `?page=1`
-     * @queryParam sort Sorting options e.g. `?sort=surname:asc,othernames:asc`
-     * @queryParam count Count related models. Alternatively `with_count` e.g. `?count=student,student_program`
-     * @queryParam contain Contain data from related model e.g. `?contain=program,currency`
+     * @queryParam limit Total items to return e.g. `?limit=15`. Example: 15
+     * @queryParam page Page of items to return e.g. `?page=1`. Example: 1
+     * @queryParam sort Sorting options e.g. `?sort=surname:asc,othernames:asc`. Example: id:asc
+     * @queryParam count Count related models. Alternatively `with_count` e.g. `?count=student,student_program`. Example: null
+     * @queryParam contain Contain data from related model e.g. `?contain=program,currency`. Example: null
      * @queryParam fieldName Pass any field and value to filter results e.g. `name=John&email=any@aol.com`
-     * 
+     *
      * @authenticated
      * @return \Illuminate\Http\Response
      */
@@ -93,9 +93,9 @@ abstract class ApiController extends BaseController
 
     /**
      * Create Resource
-     * 
-     * @queryParam count Count related models. Alternatively `with_count` e.g. `?count=student,student_program`
-     * @queryParam contain Contain data from related model e.g. `?contain=program,currency`
+     *
+     * @queryParam count Count related models. Alternatively `with_count` e.g. `?count=student,student_program`. Example: null
+     * @queryParam contain Contain data from related model e.g. `?contain=program,currency`. Example: null
      *
      * @authenticated
      * @param  \Illuminate\Foundation\Http\FormRequest  $request
@@ -118,7 +118,7 @@ abstract class ApiController extends BaseController
 
             $dataModel = $this->Model->store($request);
             return new $this->Resource($dataModel);
-            
+
         }catch (\Exception $e) {
             return response()->json([
                 'status' => 'error',
@@ -130,10 +130,10 @@ abstract class ApiController extends BaseController
     /**
      * View Resource
      *
-     * @queryParam sort Sorting options e.g. `?sort=surname:asc,othernames:asc`
-     * @queryParam count Count related models. Alternatively `with_count` e.g. `?count=student,student_program`
-     * @queryParam contain Contain data from related model e.g. `?contain=program,currency`
-     *  
+     * @queryParam sort Sorting options e.g. `?sort=surname:asc,othernames:asc`. Example: id:asc
+     * @queryParam count Count related models. Alternatively `with_count` e.g. `?count=student,student_program`. Example: null
+     * @queryParam contain Contain data from related model e.g. `?contain=program,currency`. Example: null
+     *
      * @authenticated
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -154,9 +154,9 @@ abstract class ApiController extends BaseController
 
     /**
      * Update Resource
-     * 
-     * @queryParam count Count related models. Alternatively `with_count` e.g. `?count=student,student_program`
-     * @queryParam contain Contain data from related model e.g. `?contain=program,currency`
+     *
+     * @queryParam count Count related models. Alternatively `with_count` e.g. `?count=student,student_program`. Example: null
+     * @queryParam contain Contain data from related model e.g. `?contain=program,currency`. Example: null
      *
      * @authenticated
      * @param  \Illuminate\Foundation\Http\FormRequest  $request
@@ -166,7 +166,7 @@ abstract class ApiController extends BaseController
     public function update(Request $request, $id)
     {
         try {
-            
+
             if( class_exists($this->Request) ) {
                 $formRequest = new $this->Request( $request->all() );
                 $validator = Validator::make( $request->all(), $formRequest->rules(), $formRequest->messages());
@@ -175,19 +175,19 @@ abstract class ApiController extends BaseController
                     return response()->json([
                         'status' => 'error',
                         'message' => $validator->errors()->all()
-                    ], 400); 
+                    ], 400);
                 }
             }
 
             $dataModel = $this->Model->modify($request, $id);
             return new $this->Resource($dataModel);
-        } 
+        }
         catch(NotFoundHttpException $e) {
             return response()->json([
                 'status' => 'failed',
                 'message' => 'Resource not found'
             ], 404);
-        } 
+        }
         catch (\Exception $e) {
             return response()->json([
                 'status' => 'error',
@@ -225,14 +225,14 @@ abstract class ApiController extends BaseController
 
     /**
      * Search Resources
-     * 
-     * @queryParam limit Total items to return e.g. `?limit=15`
-     * @queryParam page Page of items to return e.g. `?page=1`
-     * @queryParam sort Sorting options e.g. `?sort=surname:asc,othernames:asc`
-     * @queryParam count Count related models. Alternatively `with_count` e.g. `?count=student,student_program`
-     * @queryParam contain Contain data from related model e.g. `?contain=program,currency`
+     *
+     * @queryParam limit Total items to return e.g. `?limit=15`. Example: 15
+     * @queryParam page Page of items to return e.g. `?page=1`. Example: 1
+     * @queryParam sort Sorting options e.g. `?sort=surname:asc,othernames:asc`. Example: id:asc
+     * @queryParam count Count related models. Alternatively `with_count` e.g. `?count=student,student_program`. Example: null
+     * @queryParam contain Contain data from related model e.g. `?contain=program,currency`. Example: null
      * @queryParam fieldName Pass any field and value to search by e.g. `name=John&email=any@aol.com`. Search logic may use LIKE or `=` depending on field
-     * 
+     *
      * @authenticated
      */
     public function search(Request $request) {
@@ -243,9 +243,9 @@ abstract class ApiController extends BaseController
 
     /**
      * Count Resources
-     * 
-     * @queryParam fieldName Pass any field and value to search by e.g. `name=John&email=any@aol.com`. Search logic may use LIKE or `=` depending on field
-     * 
+     *
+     * @queryParam fieldName Pass any field and value to search by e.g. `name=John&email=any@aol.com`. Search logic may use LIKE or `=` depending on field. Example: null
+     *
      * @authenticated
      */
     public function count(Request $request) {
