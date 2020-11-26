6ac76f10333cb2064c2ef267628bca548bede4fa
Francis Adu-Gyamfi <francis.adu-gyamfi@topgolf.com>
2020-11-26 20:06:40 +0000

    Clean up

--- src/Http/Requests/ApiRequest.php
+++ src/Http/Requests/ApiRequest.php
@@ -27,7 +27,7 @@ class ApiRequest extends FormRequest implements ApiFormRequest
         return [];
     }
 
-    public function bodyParameters(): array {
+    public function bodyParameters() {
         return [];
     }
 }
